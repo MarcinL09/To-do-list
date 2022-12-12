99ea73345475337d1d93a11d3d7e902d926d94b2
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-12-12 18:06:14 +0100

    added 'moveDivElementToDoingSection' function

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let deleteButton;
 let rightArrowButton;
 let leftArrowButton;
 let newDiv;
+let clonedNewDiv;
 
 const toDo = document.querySelector('.to-do');
 const doing = document.querySelector('.doing');
@@ -15,12 +16,37 @@ function addNewToDo() {
         newDiv = document.createElement('div');
         newDiv.classList.add('new-div');
         toDo.append(newDiv);
-        newDiv.append(inputElement.value);
-        createToolsAreaForToDo();
+        newDiv.textContent = inputElement.value;
         inputElement.value = '';
     }
+    moveNewDivElementToDoingSection()
 }
+function moveNewDivElementToDoingSection() {
 
+    rightArrowButton = document.createElement('button')
+    rightArrowButton.classList.add('arrow');
+    rightArrowButton.innerHTML = '<i class="fa-solid fa-arrow-right"></i>';
+    newDiv.append(rightArrowButton);
+    const clonedNewDiv = newDiv.cloneNode(true);
+    clonedNewDiv.classList.add('new-div')
+
+    deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    newDiv.append(deleteButton);
+
+    leftArrowButton = document.createElement('button')
+    leftArrowButton.classList.add('left-arrow');
+    leftArrowButton.innerHTML = '<i class="fa-solid fa-arrow-left"></i>';
+
+    rightArrowButton.addEventListener('click', function (){
+        const clonedNewDiv = newDiv.cloneNode(true);
+        doing.append(clonedNewDiv);
+        newDiv.remove();
+    })
+
+    deleteButton.addEventListener('click', deleteClick);
+}
 
 function deleteClick(event) {
     event.target.closest('.new-div').remove();
